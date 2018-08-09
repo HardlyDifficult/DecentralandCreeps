721c909562aceacbbb50bb8a8228b424d671be7c
HardlyDifficult <HardlyDifficultLLC@gmail.com>
2018-08-09 15:10:31 -0700

    Remove build

--- server/build/ConnectedClients.js
+++
@@ -1,7 +0,0 @@
-export const connectedClients = new Set();
-export function updateAll() {
-    connectedClients.forEach(function each(client) {
-        client.forceUpdate();
-    });
-}
-//# sourceMappingURL=ConnectedClients.js.map
--- server/build/components/Tile.js
+++
@@ -1,6 +0,0 @@
-import * as DCL from 'metaverse-api';
-import { Helpers } from '../Helpers';
-export const Tile = (props) => {
-    return (DCL.createElement("plane", { position: Helpers.plusVector3(Helpers.gridToWorld(props.gridPosition), { x: 0, y: .001, z: 0 }), material: "#floorTileMaterial", rotation: { x: 90, y: 0, z: 0 } }));
-};
-//# sourceMappingURL=Tile.js.map
